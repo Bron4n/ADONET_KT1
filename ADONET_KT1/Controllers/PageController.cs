32e65de89c583c287c2654a1b785d7ce2fc907ab
user <user@DESKTOP-334GVLS>
2025-01-20 16:39:30 +1000

    Добавьте файлы проекта.

---
+++ ADONET_KT1/Controllers/PageController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace ADONET_KT1.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Сообщение не может быть пустым!";
+            }
+            else
+            {
+                ViewData["Message"] = $"Вы отправили сообщение: {message}";
+            }
+
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+        public IActionResult Greet(string name)
+        {
+            ViewData["Name"] = name ?? "Гость";
+            return View();
+        }
+
+    }
+}
+
+
---
+++ ADONET_KT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADONET_KT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
